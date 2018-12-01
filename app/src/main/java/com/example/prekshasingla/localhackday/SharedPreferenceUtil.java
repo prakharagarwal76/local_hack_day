deb652e5c68e28a21fb64a38c3252ed3b4a11c61
prakhar agarwal <prakharagarwal76@gmail.com>
2018-12-01 13:57:00 +0530

    login screen added with firebase

---
+++ app/src/main/java/com/example/prekshasingla/localhackday/SharedPreferenceUtil.java
@@ -0,0 +1,41 @@
+package com.example.prekshasingla.localhackday;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferenceUtil {
+
+    private static SharedPreferenceUtil mSharedPreferenceUtils;
+    protected Context mContext;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
+
+    private SharedPreferenceUtil(Context context) {
+        mContext = context;
+        mSharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+    }
+
+    public static synchronized SharedPreferenceUtil getInstance(Context context) {
+
+        if (mSharedPreferenceUtils == null) {
+            mSharedPreferenceUtils = new SharedPreferenceUtil(context.getApplicationContext());
+        }
+        return mSharedPreferenceUtils;
+    }
+
+    public void setType(int value) {
+        mSharedPreferencesEditor.putInt("loginType", value);
+        mSharedPreferencesEditor.commit();
+    }
+
+
+    public int getType() {
+        return mSharedPreferences.getInt("loginType", 0);
+    }
+
+
+    public void clear() {
+        mSharedPreferencesEditor.clear().commit();
+    }
+}
